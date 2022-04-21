26233a9c6b80de12a18564b6db0f38283a4d3109
youngeun choi <choi46910@naver.com>
2022-04-21 10:57:14 +0900

    jango blog 내용 추가

--- landing/urls.py
+++ landing/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
   path('', views.index),
   path('<int:month>/', views.months),
-  # path('<str:name>/', views.detail)
+  path('<str:name>/', views.detail)
   
   #/landing/hooni
   # hooni,email,취미
